a2fd9bc4b45e665f5925c7ac484207a384d42519
codenameglassy <codenameglassy@gmail.com>
2022-08-11 11:04:09 +0545

    g

--- changa/Assets/Script/KiteMovement.cs
+++ changa/Assets/Script/KiteMovement.cs
@@ -100,8 +100,14 @@ public class KiteMovement : MonoBehaviour
 
     }
 
-    
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Enemy"))
+        {
+            Destroy(gameObject);
+        }
+    }
 
-   
-   
+
+
 }
