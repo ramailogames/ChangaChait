a1ffcf827328d28ca80cb7f355d2479ce6686b2d
codenameglassy <codenameglassy@gmail.com>
2022-12-09 10:31:14 +0545

    g

--- changa/Assets/Script/CreateAd.cs
+++ changa/Assets/Script/CreateAd.cs
@@ -5,19 +5,39 @@ using UnityEngine;
 public class CreateAd : MonoBehaviour
 {
 
+    private void Awake()
+    {
+        Application.runInBackground = true; 
+    }
     private void OnEnable()
     {
-        Ad();
+        FindObjectOfType<JsFuncManager>().CreateAd();
+        FindObjectOfType<JsFuncManager>().CreateAd();
     }
 
-    private void Ad()
-    {
-        FindObjectOfType<JsFuncManager>().CreateAd();
-        FindObjectOfType<JsFuncManager>().CreateAd();
-    }
 
     private void Start()
     {
-        Invoke("Ad", 1f);
+        Invoke("CreateAd_", .1f);
+        Invoke("CreateAd_", .2f);
+       
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            ResumeGame();
+        }
+    }
+    public void CreateAd_()
+    {
+        FindObjectOfType<JsFuncManager>().CreateAd();
+        Invoke("ResumeGame", 1f);
+    }
+
+    void ResumeGame()
+    {
+        FindObjectOfType<JsFuncManager>().ReturnFocus();
     }
 }
